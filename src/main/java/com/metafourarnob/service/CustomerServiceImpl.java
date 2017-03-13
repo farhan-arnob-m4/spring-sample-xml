5c8a396e33e8a96726f70a7b38d261ee246a4807
farhan-arnob-m4 <farhanarnob@metafour.com>
2017-03-13 14:16:30 +0600

    Change in application context.

--- src/main/java/com/metafourarnob/service/CustomerServiceImpl.java
+++ src/main/java/com/metafourarnob/service/CustomerServiceImpl.java
@@ -24,7 +24,7 @@ public class CustomerServiceImpl implements CustomerService {
 		return customerRepository.findAll();
 	}
 
-	public void setFoo(CustomerRepository customerRepository) {
+	public void setCustomerRepository(CustomerRepository customerRepository) {
 		this.customerRepository = customerRepository;
 	}
 	
