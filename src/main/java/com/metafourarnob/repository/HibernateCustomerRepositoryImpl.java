4fd00f0e074defd2ad8f78749e0b35e6d3849137
farhan-arnob-m4 <farhan.arnob@metafour.com>
2017-03-14 14:09:03 +0600

    Value from properties has been implemented in xml based configuration.

--- src/main/java/com/metafourarnob/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/metafourarnob/repository/HibernateCustomerRepositoryImpl.java
@@ -6,12 +6,19 @@ import java.util.List;
 import com.metafourarnob.model.Customer;
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
+	
+	private String dbUserName;
+
+	public void setDbUserName(String dbUserName) {
+		this.dbUserName = dbUserName;
+	}
 
 	/* (non-Javadoc)
 	 * @see com.metafourarnob.repository.CustomerRepository#findAll()
 	 */
 	@Override
 	public List<Customer> findAll() {
+		System.out.println(dbUserName);
 		List<Customer> customers = new ArrayList<>();
 		Customer customer = new Customer();
 		customer.setFirstName("Alandi");
@@ -20,5 +27,7 @@ public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 		return customers;
 
 	}
+
+	
 
 }
